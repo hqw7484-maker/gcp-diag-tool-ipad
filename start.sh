4614722ee6553bd6f60d4f6e7b6c9ca25cc8cb47
H QW <hqw7484@gmail.com>
2026-04-06 13:00:39 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 RAW_URL="https://raw.githubusercontent.com/hqw7484-maker/gcp-diag-tool-ipad/main"
 
-# 执行部署
+# 1. 执行部署并吐链接
 curl -sL "$RAW_URL/setup.sh" | bash
 
-# 进入监控
+# 2. 只有 setup 成功结束后，才进入保活监控
 curl -sL "$RAW_URL/monitor.sh" | bash
