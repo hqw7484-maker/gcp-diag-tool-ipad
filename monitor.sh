2572768f1da06bec7cb7f9601efaa4809e889e47
H QW <hqw7484@gmail.com>
2026-04-06 11:29:33 +0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -5,7 +5,6 @@ while true; do
     TIME=$(date '+%H:%M:%S')
     printf "\r\033[K"
     echo -ne "\033[36m[iPad Guard]\033[0m 时间: $TIME | 状态: \033[32mACTIVE\033[0m | 链接: $LINK"
-    # 模拟文件操作，保持磁盘 IO 活动
     touch .keep_alive
     sleep 60
 done
