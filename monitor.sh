22a72582d344185ae2f552bb45c4d66d0f3c2102
H QW <hqw7484@gmail.com>
2026-04-06 12:50:42 +0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+clear
 while true; do
-    LINK=$(grep -o 'https://[-0-9a-z]*\.trycloudflare.com' cf.log 2>/dev/null | head -n 1)
     TIME=$(date '+%H:%M:%S')
-    echo -ne "\r\033[1;36m[iPad Guard]\033[0m \033[33m$TIME\033[0m | \033[32mACTIVE\033[0m | \033[35m$LINK\033[0m"
+    # 动态抓取最新链接
+    LINK=$(grep -iE "https://[a-zA-Z0-9.-]+\.trycloudflare\.com" cf.log | grep -oE "https://[a-zA-Z0-9.-]+\.trycloudflare\.com" | head -n 1)
+    
+    echo -ne "\r\033[1;36m[iPad Guard]\033[0m \033[33m$TIME\033[0m | \033[32mACTIVE\033[0m | \033[35m${LINK:-Searching...}\033[0m"
+    
     touch .keep_alive
-    sleep 60
+    sleep 30
 done
